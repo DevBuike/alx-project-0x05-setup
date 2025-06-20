c96c2755136597703ac311eae58211a7d6935ecd
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-06-20 18:10:27 +0000

    adding generate-image.ts

--- alx-project-0x09/pages/index.tsx
+++ alx-project-0x09/pages/index.tsx
@@ -7,15 +7,29 @@ import { useState } from "react";
 const Home: React.FC = () => {
   const [prompt, setPrompt] = useState<string>("");
   const [imageUrl, setImageUrl] = useState<string>("");
-  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>(
-    []
-  );
   const [isLoading, setIsLoading] = useState<boolean>(false)
 
 
    const handleGenerateImage = async () => {
-    console.log("Generating Image")
-    console.log(process.env.NEXT_PUBLIC_GPT_API_KEY)
+    setIsLoading(true);
+    const resp = await fetch('/api/generate-image', {
+      method: 'POST',
+      body: JSON.stringify({
+        prompt
+      }),
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+
+
+    if (!resp.ok) {
+      setIsLoading(false)
+      return;
+    }
+
+    const data = await resp.json()
+    setIsLoading(false)
   };
 
   return (
